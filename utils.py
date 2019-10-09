de121375c9dc9ba63a03746dc05aeae9325c7195
unknown <ik-jochem@hotmail.com>
2019-10-09 15:24:32 +0200

    some adjustments

---
+++ utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+# Returns EAR given eye landmarks
+def eye_aspect_ratio(eye):
+    # Compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = np.linalg.norm(eye[1] - eye[5])
+    B = np.linalg.norm(eye[2] - eye[4])
+
+    # Compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = np.linalg.norm(eye[0] - eye[3])
+
+    # Compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # Return the eye aspect ratio
+    return ear
+
+
+# Returns MAR given eye landmarks
+def mouth_aspect_ratio(mouth):
+    # Compute the euclidean distances between the three sets
+    # of vertical mouth landmarks (x, y)-coordinates
+    A = np.linalg.norm(mouth[13] - mouth[19])
+    B = np.linalg.norm(mouth[14] - mouth[18])
+    C = np.linalg.norm(mouth[15] - mouth[17])
+
+    # Compute the euclidean distance between the horizontal
+    # mouth landmarks (x, y)-coordinates
+    D = np.linalg.norm(mouth[12] - mouth[16])
+
+    # Compute the mouth aspect ratio
+    mar = (A + B + C) / (2 * D)
+
+    # Return the mouth aspect ratio
+    return mar
+
+
+# Return direction given the nose and anchor points.
+def direction(nose_point, anchor_point, w, h, multiple=1):
+    nx, ny = nose_point
+    x, y = anchor_point
+
+    # Calculate for each direction the distance to the square
+    left = nx < x - w
+    left = nx - (x - w) if left else 0
+    right = nx > x + w
+    right = nx - (x + w) if right else 0
+    down = ny < y - h
+    down = ny - (y - h) if down else 0
+    up = ny > y + h
+    up = ny - (y + h) if up else 0
+
+    return np.array([left + right, down + up])
